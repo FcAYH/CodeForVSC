1d6ef6453fb5c2d0f48a864a61213494da05941a
FcAYH <1473988037@qq.com>
2021-04-16 17:13:32 +0800

    20214/16/513

--- capture.py
+++ capture.py
@@ -1,5 +1,5 @@
 import cv2
-cap = cv2.VideoCapture(0)  # 设置摄像头 0是默认的摄像头 如果你有多个摄像头的话呢，可以设置1,2,3....
+cap = cv2.VideoCapture(1)  # 设置摄像头 0是默认的摄像头 如果你有多个摄像头的话呢，可以设置1,2,3....
 while True:  # 进入无限循环
     ret, frame = cap.read()  # 将摄像头拍到的图像作为frame值
     cv2.imshow('frame', frame)  # 将frame的值显示出来 有两个参数 前一个是窗口名字，后面是值
