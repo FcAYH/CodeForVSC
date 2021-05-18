eb8ffb984e4674c29b3dbac11bdd43da2088b9bf
FcAYH <1473988037@qq.com>
2021-05-19 02:36:12 +0800

    2021/5/19/236

--- 421.数组中两个数的最大异或值.cs
+++ 421.数组中两个数的最大异或值.cs
@@ -15,25 +15,49 @@ public class Solution
         int root = 0;
         for (int i = 31; i >= 0; i--)
         {
-            if (x & (1 << i) != 0)
+            if ((x & (1 << i)) != 0)
             {
-                if (trie[root][1] == 0)
-                    trie[root][1] = ++count;
-                root = trie[root][1];
+                if (trie[root, 1] == 0)
+                    trie[root, 1] = ++count;
+                root = trie[root, 1];
             }
             else
             {
-                if (trie[root][0] == 0)
-                    trie[root][0] = ++count;
-                root = trie[root][0];
+                if (trie[root, 0] == 0)
+                    trie[root, 0] = ++count;
+                root = trie[root, 0];
             }
-
         }
     }
 
+    private bool Find(int x, int k)
+    {
+        int root = 0;
+        for (int i = 31; i >= 0; i--)
+        {
+            if ((x & (1 << i)) != 0)
+            {
+                if (trie[root, 1] == 0)
+                    return false;
+
+                root = trie[root, 1];
+            }
+            else
+            {
+                if (trie[root, 0] == 0)
+                    return false;
+
+                root = trie[root, 0];
+            }
+
+            if (i == k) return true;
+        }
+
+        return true;
+    }
     public int FindMaximumXOR(int[] nums)
     {
-        trie = new int[nums.Length * 31, 2];
+        trie = new int[nums.Length * 32, 2];
 
         foreach (int i in nums)
             BuildNode(i);
@@ -43,7 +67,7 @@ public class Solution
         for (int digit = 31; digit >= 0; digit--)
         {
             foreach (int i in nums)
-                if (Find((Ans | (1 << digit)) ^ i))
+                if (Find((Ans | (1 << digit)) ^ i, digit))
                     Ans |= (1 << digit);
         }
 
