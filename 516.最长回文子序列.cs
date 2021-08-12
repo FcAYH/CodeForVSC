553ccd656aa4090737bbb0909fc703f413e92cc7
FcAYH <1473988037@qq.com>
2021-08-12 21:54:04 +0800

    2021/8/12/2154

--- 516.最长回文子序列.cs
+++ 516.最长回文子序列.cs
@@ -9,7 +9,24 @@ public class Solution
 {
     public int LongestPalindromeSubseq(string s)
     {
+        int length = s.Length;
+        int[,] A = new int[length, length];
 
+        for (int i = 0; i < length; i++)
+            A[i, i] = 1;
+
+        for (int j = 1; j < length; j++)
+        {
+            for (int i = 0; i < j; i++)
+            {
+                if (s[i] == s[j])
+                    A[i, j] = A[i + 1, j - 1] + 1;
+
+                A[i, j] = Math.Max(A[i + 1, j], A[i, j + 1]);
+            }
+        }
+
+        return A[0, length - 1];
     }
 }
 // @lc code=end
