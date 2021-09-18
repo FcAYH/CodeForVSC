846003e704b5d729191e218a15cffc4c4ee8f81b
FcAYH <1473988037@qq.com>
2021-09-18 20:18:46 +0800

    2021/9/18/2018

---
+++ 292.nim-游戏.cs
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode.cn id=292 lang=csharp
+ *
+ * [292] Nim 游戏
+ */
+
+// @lc code=start
+public class Solution
+{
+    public bool CanWinNim(int n)
+    {
+        if (n % 4 == 0)
+            return false;
+
+        else return true;
+
+    }
+}
+// @lc code=end
+
