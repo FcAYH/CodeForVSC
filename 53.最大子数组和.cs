e8ab9f1ff0171485697d02449747f00e05f6e509
FcAYH <1473988037@qq.com>
2022-01-12 22:06:57 +0800

    53.最大子段和

--- 53.最大子数组和.cs
+++ 53.最大子数组和.cs
@@ -5,9 +5,29 @@
  */
 
 // @lc code=start
-public class Solution {
-    public int MaxSubArray(int[] nums) {
+public class Solution
+{
+    const int Inf = 0x3f3f3f3f;
+    public int MaxSubArray(int[] nums)
+    {
+        // int[] dp = new int[nums.Length];
+        // dp[0] = nums[0];
 
+        // int ans = dp[0];
+        // for (int i = 1; i < nums.Length; i++)
+        // {
+        //     dp[i] = Math.Max(dp[i - 1] + nums[i], nums[i]);
+        //     ans = Math.Max(ans, dp[i]);
+        // }
+
+        int dpVal = nums[0], ans = nums[0];
+        for (int i = 1; i < nums.Length; i++)
+        {
+            dpVal = Math.Max(dpVal + nums[i], nums[i]);
+            ans = Math.Max(ans, dpVal);
+        }
+
+        return ans;
     }
 }
 // @lc code=end
