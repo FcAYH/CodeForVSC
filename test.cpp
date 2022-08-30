8401caa873b7d8c4c2d6396eea65b9b710ed0796
F_CIL <F_CIL@outloook.com>
2022-08-30 10:01:30 +0800

    新电脑配置调整

--- test.cpp
+++ test.cpp
@@ -1,32 +1,15 @@
-#include <iostream>
-#include <cmath>
+#include <bits/stdc++.h>
+
 using namespace std;
-int max_product(int x)
+
+void Solve()
 {
-    if (x == 1)
-        return 1;
-    if (x == 2)
-        return 1;
-    if (x == 3)
-        return 2;
-    if (x >= 4)
-    {
-        long long int result;
-        int m = x % 3; // m为余数
-        int n = x / 3; // n为商
-        if (m == 0)
-            result = pow(3, n);
-        else if (m == 1)
-            result = pow(3, n - 1) * 4;
-        else
-            result = pow(3, n) * 2;
-        return result;
-    }
+    printf("Hello World");
 }
+
 int main()
 {
-    int y;
-    cin >> y;
-    cout << max_product(y) << endl;
+    Solve();
+
     return 0;
 }
