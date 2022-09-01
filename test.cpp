b3fe148f5e264ac72bbdf4b76d5b9e667e48eaa9
F_CIL <F_CIL@outloook.com>
2022-09-01 15:24:02 +0800

    c++语法

--- test.cpp
+++ test.cpp
@@ -5,32 +5,53 @@ using namespace std;
 class Test
 {
 public:
-    Test()
+    int val;
+    char *str;
+
+public:
+    Test(int x) : val(x)
     {
         cout << "ctor" << endl;
     }
 
     ~Test()
     {
-        cout << "free" << endl;
+        cout << "val: " << val << " free" << endl;
+    }
+
+    void ctor()
+    {
+        str = (char *)malloc(sizeof(char) * 6);
+        cin >> str;
     }
 
     void fun1()
     {
-        delete this;
+        // delete this;
         cout << "hello world" << endl;
+    }
+
+    virtual void fun2()
+    {
+        cout << "virtual fun" << endl;
     }
 };
 
 void Solve()
 {
-    Test *p = new Test();
-    delete p;
-    p->fun1();
-    delete p;
+    // Test *p = (Test *)malloc(sizeof(p));
+    // p->ctor();
+    // free(p);
+    // cout << p->str;
+    // Test *p = new Test(2);
+    // delete p;
+    // p->fun1();
+    // delete p;
 
     Test *q = nullptr;
+    // cout << q->val << endl;
     q->fun1();
+    q->fun2();
     delete q;
 }
 
