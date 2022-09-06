6631017acf946d233e8cf35c844142eeee830d4e
FcAYH <1473988037@qq.com>
2022-09-06 11:06:01 +0800

    文章整理

--- firsttrt.md
+++ firsttrt.md
@@ -1,164 +1,196 @@
-## 我的Markdown
+# C++类的实现
 
-```
-/*
- * @Author: F_CIL 
- * @Date: 2021-08-23 10:53:22 
- * @Last Modified by: F_CIL
- * @Last Modified time: 2021-08-23 10:53:46
- */
-```
-[TOC]
+> 事情是因为面试的时候，被问到了一些奇奇怪怪的用法，所以来研究了一下C++中类对象在内存中是怎么存储的。
 
-### 第一章 标题测试
+* [ ] 这一块内容挺多的，有必要单独开个文章写
 
-##### 这次是小标题
+## malloc和new的区别
 
-- nihao
-  - nihao
-    - nihao
-      - sd
+先记录一下malloc和new的区别：
 
-1. sda
-2. fddf  
+1. malloc返回一个void\*类型的指针，需要自己进行强转。而new根据指定类型返回指定类型的指针。
 
-你好
+2. malloc需要自己指定分配内存的大小，以参数形式传入。而new不需要。
 
-我好
+3. malloc是函数，而new是运算符。new可以被重载。
 
-nihao[^1]
+4. malloc不会调用类的构造函数，而new会调用。同理，free不会调用类的析构函数，而delete会。
 
+## 奇怪的写法
 
-[^1]: https://github.com/
+> 当然了，个人认为这一系列的写法，都不符合标准，不管是在学习还是在生产中都应该避免使用上述写法。但是有时研究一下这些写法的结果，会让我们对C++的理解更加深刻。
 
-### 第二章 代码测试
+首先我们定义类如下：
 
 ```cpp
-#include <iostream>
+class CppTest
+{
+public:
+    int val;
+
+    CppTest(int x) : val(x) {}
+    ~CppTest()
+    {
+        cout << "free" << endl;
+    }
+
+    void FunA()
+    {
+        cout << "Hello world" << endl;
+    }
+
+    void FunB()
+    {
+        cout << "Good night" << endl;
+    }
+};
 
 int main()
 {
-    printf("Hello World!\n");
+    CppTest* p = new CppTest(2);
+
+    p->FunA();
+
+    delete p;
 
     return 0;
 }
 ```
-$1 \le n \le 10^4$
 
-$$ \sum_{i = 0}^{10^5} f(n)$$
+### 在析构函数中delete
 
-:punch:	
+如果我们魔改一下析构函数，改成下面这样子：
 
-- 上标：$30^th$
-  
-- 下标：$H_2O$
-- 脚注：Content [^2]
-
-- [ ] 这是一个测试用的
-
-[^2]:Hi 这里是一个注脚，会自动拉到最后面排版
-<table>
-    <tr>
-        <th rowspan="2">值班人员</th>
-        <th>星期一</th>
-        <th>星期二</th>
-       <th>星期三</th>
-    </tr>
-    <tr>
-        <td>李强</td>
-        <td>张明</td>
-        <td>王平</td>
-    </tr>
-</table>
-
-st=>start: Start:>https://www.zybuluo.com
-io=>inputoutput: verification
-op=>operation: Your Operation
-cond=>condition: Yes or No?
-sub=>subroutine: Your Subroutine
-e=>end
-
-st->io->op->cond
-cond(yes)->e
-cond(no)->sub->io
-
-```gantt
-
-    title 项目开发流程
-    section 项目确定
-        需求分析       :a1, 2016-06-22, 1d
-        可行性报告     :a2, after a1, 1d
-        概念验证       :a3, after a2, 1d
-    section 项目实施
-        概要设计      :2016-07-05  , 5d
-        详细设计      :2016-07-08, 10d
-        编码          :2016-07-15, 10d
-        测试          :2016-07-22, 5d
-    section 发布验收
-        发布: 2d
-        验收: 3d
+```cpp
+    ~CppTest()
+    {
+        cout << "free" << endl;
+        delete this;
+    }
 ```
-### 第三章 画图测试
 
-`sdd `
+会发生什么事？
 
-```dot
-digraph G
+### 在成员函数中delete
+
+如果我们魔改一下FunA，改成下面这样子：
+
+```cpp
+    void FunA()
+    {
+        delete this;
+        cout << "Hello world" << endl;
+    }
+```
+
+会发生什么事？
+
+如果再魔改一下，这样子：
+
+```cpp
+    void FunA()
+    {
+        delete this;
+        cout << "Hello world" << endl;
+
+        FunB();
+    }
+```
+
+又会有怎么样的输出呢？
+
+### 空指针能否调用函数？
+
+如果我们修改调用方式如下：
+
+```cpp
+int main()
 {
-    A -> B
-    B -> C
-    B -> D
-    D -> E
+    CppTest* p = nullpter;
+    p->FunA();
+
+    return 0;
 }
 ```
 
+能否成功调用？
 
+## 解答
+
+**1\. 当在析构函数中调用delete时，会触发循环递归，导致程序无法正常退出**
+
+首先delete会调用析构函数，故而`delete p;`会跳转到我们的`~CppTest()`，然而我们在`~CppTest()`中又触发`delete this`，就导致了又跳转回了`~CppTest()`，反复循环递归。
+
+**2\. 在成员函数中delete，并不会中断成员函数的运行，也不会影响再调用其他成员函数**
+
+即在`FunA()`中，运行`delete this;`，程序仍然会照常输出`Hello world`，同时也能正常调用`FunB()`，输出`Good night`。
+
+这个现象我们就能够猜测，对于一个类对象，该对象本身并不会存储成员函数相关的内容，所以即使`delete this;`，CppTest类型的指针依然可以毫无障碍的调用到FunA，FunB。
+
+**3\. nullpter也可以调用到成员函数**
+
+即`CppTest* p = nullpter;`，`p->FunA();`可以输出`Hello world`。
+
+基于这个现象我们基本就可以猜测，在C++中，成员函数的实现，应该就是在调用时，隐含着将自身作为参数传入。只要是CppTest类型的指针，不管有没有真正实例化一个对象，都可以调用成员函数。
+
+## 再做几组测试
+
+### delete后调用val
 
 ```cpp
-威威
+int main()
+{
+    CppTest* p = new CppTest(2);
+    delete p;
+    cout << p->val << endl;
+}
 ```
-- first
-- second
-- third
 
-- first
-- second
-- third
+### 添加一个虚函数，并在delete后调用
 
-[text](https://link)
-1. first
-2. second
-3. third
+```cpp
+...
+    virtual void FunVirtual()
+    {
+        cout << "Virtual function" << endl;
+    }
+...
 
-- first
-- second
-- third:::image type="content" source="Altest2.drawio.png" alt-text="jj":::
+int main()
+{
+    CppTest* p = new CppTest(2);
+    delete p;
+    p->FunVirtual();
+}
+```
 
-wd
-: sdsad
-**sdsds**
+### nullpter调用用val和虚函数
 
-![alt](https://link)
+```cpp
+int main()
+{
+    CppTest* p = nullpter;
+    cout << p->val << endl;
+    // p->FunVirtual(); 这两行二选一执行一下
+}
+```
 
-> sds
+### 验证
 
-### 第四章 表格测试
+前两个运行一下我们发现，delete了之后，仍然可以正确输出val的值，以及调用虚函数可以看到输出了`Virtual function`。这里我们能够猜测，delete仅仅只是标记了这块内存可以被再次使用，并没有清理这块内存的内容。
 
-**hsdasd**
+第三个我们发现运行起来会崩溃，结合之前的猜测，我们可以得知，类对象内存中包含的就是类的成员变量，同时还有虚函数表指针。
 
-| Column1 | Column2  | Column3   |
-| ------- | -------- | --------- |
-| Row1    | dfgdfgdf | dfgdfgdfg |
-| Row2    | dg       | g         |
-| Row3    | d        | d         |
-| Row4    | d        | g         |
+想要去验证的话自然是应该去看编译后的汇编代码，以及代码运行时的内存分配情况。
 
-:::row:::
-    :::column:::dfgdfgdf
-        sdfdsf
-    :::column-end:::
-    :::column:::
-        sdfsdfs
-    :::column-end:::
-:::row-end:::
+## 结论
+
+1. 类中成员函数保存在代码段中，只要是该类型的指针，就可以调用该类型的普通成员函数。实例化一个对象时，占用内存包括指向虚函数表的指针以及成员变量。
+
+2. delete并不会清理内存，只是会标记该区域可再次被分配，同理new操作本身也不会清理内存。故而我们最好在构造函数中完成对各个参数的初始化。
+
+3. 如果类中包含虚函数，则内存最开始是一个指向虚函数表的指针。而虚函数表的存储，在不同的编译器中不太相同，微软的编译器将其放在了常量数据段，有的则在只读数据段。（从网上查阅，以后自己去确认一下看看）
+
+
+
 
---
+++ friend.s
@@ -0,0 +1,375 @@
+	.file	"friend.cpp"
+	.text
+	.section .rdata,"dr"
+__ZStL19piecewise_construct:
+	.space 1
+__ZStL13allocator_arg:
+	.space 1
+__ZStL6ignore:
+	.space 1
+.lcomm __ZStL8__ioinit,1,1
+	.align 4
+__ZN9__gnu_cxxL21__default_lock_policyE:
+	.long	2
+__ZStL10defer_lock:
+	.space 1
+__ZStL11try_to_lock:
+	.space 1
+__ZStL10adopt_lock:
+	.space 1
+	.align 4
+__ZNSt15regex_constantsL5icaseE:
+	.long	1
+	.align 4
+__ZNSt15regex_constantsL6nosubsE:
+	.long	2
+	.align 4
+__ZNSt15regex_constantsL8optimizeE:
+	.long	4
+	.align 4
+__ZNSt15regex_constantsL7collateE:
+	.long	8
+	.align 4
+__ZNSt15regex_constantsL10ECMAScriptE:
+	.long	16
+	.align 4
+__ZNSt15regex_constantsL5basicE:
+	.long	32
+	.align 4
+__ZNSt15regex_constantsL8extendedE:
+	.long	64
+	.align 4
+__ZNSt15regex_constantsL3awkE:
+	.long	128
+	.align 4
+__ZNSt15regex_constantsL4grepE:
+	.long	256
+	.align 4
+__ZNSt15regex_constantsL5egrepE:
+	.long	512
+	.align 4
+__ZNSt15regex_constantsL12__polynomialE:
+	.long	1024
+	.align 4
+__ZNSt15regex_constantsL13match_defaultE:
+	.space 4
+	.align 4
+__ZNSt15regex_constantsL13match_not_bolE:
+	.long	1
+	.align 4
+__ZNSt15regex_constantsL13match_not_eolE:
+	.long	2
+	.align 4
+__ZNSt15regex_constantsL13match_not_bowE:
+	.long	4
+	.align 4
+__ZNSt15regex_constantsL13match_not_eowE:
+	.long	8
+	.align 4
+__ZNSt15regex_constantsL9match_anyE:
+	.long	16
+	.align 4
+__ZNSt15regex_constantsL14match_not_nullE:
+	.long	32
+	.align 4
+__ZNSt15regex_constantsL16match_continuousE:
+	.long	64
+	.align 4
+__ZNSt15regex_constantsL16match_prev_availE:
+	.long	128
+	.align 4
+__ZNSt15regex_constantsL14format_defaultE:
+	.space 4
+	.align 4
+__ZNSt15regex_constantsL10format_sedE:
+	.long	256
+	.align 4
+__ZNSt15regex_constantsL14format_no_copyE:
+	.long	512
+	.align 4
+__ZNSt15regex_constantsL17format_first_onlyE:
+	.long	1024
+	.align 4
+__ZNSt15regex_constantsL13error_collateE:
+	.space 4
+	.align 4
+__ZNSt15regex_constantsL11error_ctypeE:
+	.long	1
+	.align 4
+__ZNSt15regex_constantsL12error_escapeE:
+	.long	2
+	.align 4
+__ZNSt15regex_constantsL13error_backrefE:
+	.long	3
+	.align 4
+__ZNSt15regex_constantsL11error_brackE:
+	.long	4
+	.align 4
+__ZNSt15regex_constantsL11error_parenE:
+	.long	5
+	.align 4
+__ZNSt15regex_constantsL11error_braceE:
+	.long	6
+	.align 4
+__ZNSt15regex_constantsL14error_badbraceE:
+	.long	7
+	.align 4
+__ZNSt15regex_constantsL11error_rangeE:
+	.long	8
+	.align 4
+__ZNSt15regex_constantsL11error_spaceE:
+	.long	9
+	.align 4
+__ZNSt15regex_constantsL15error_badrepeatE:
+	.long	10
+	.align 4
+__ZNSt15regex_constantsL16error_complexityE:
+	.long	11
+	.align 4
+__ZNSt15regex_constantsL11error_stackE:
+	.long	12
+	.align 4
+__ZNSt8__detailL19_S_invalid_state_idE:
+	.long	-1
+	.section	.text$_ZN7CppTestC1Ei,"x"
+	.linkonce discard
+	.align 2
+	.globl	__ZN7CppTestC1Ei
+	.def	__ZN7CppTestC1Ei;	.scl	2;	.type	32;	.endef
+__ZN7CppTestC1Ei:
+LFB7827:
+	.cfi_startproc
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	subl	$4, %esp
+	movl	%ecx, -4(%ebp)
+	movl	-4(%ebp), %eax
+	movl	8(%ebp), %edx
+	movl	%edx, (%eax)
+	nop
+	leave
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
+	ret	$4
+	.cfi_endproc
+LFE7827:
+	.section .rdata,"dr"
+LC0:
+	.ascii "free\0"
+	.section	.text$_ZN7CppTestD1Ev,"x"
+	.linkonce discard
+	.align 2
+	.globl	__ZN7CppTestD1Ev
+	.def	__ZN7CppTestD1Ev;	.scl	2;	.type	32;	.endef
+__ZN7CppTestD1Ev:
+LFB7830:
+	.cfi_startproc
+	.cfi_personality 0,___gxx_personality_v0
+	.cfi_lsda 0,LLSDA7830
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	subl	$40, %esp
+	movl	%ecx, -12(%ebp)
+	movl	$LC0, 4(%esp)
+	movl	$__ZSt4cout, (%esp)
+	call	__ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc
+	movl	$__ZSt4endlIcSt11char_traitsIcEERSt13basic_ostreamIT_T0_ES6_, (%esp)
+	movl	%eax, %ecx
+	call	__ZNSolsEPFRSoS_E
+	subl	$4, %esp
+	nop
+	leave
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+LFE7830:
+	.def	___gxx_personality_v0;	.scl	2;	.type	32;	.endef
+	.section	.gcc_except_table,"w"
+LLSDA7830:
+	.byte	0xff
+	.byte	0xff
+	.byte	0x1
+	.uleb128 LLSDACSE7830-LLSDACSB7830
+LLSDACSB7830:
+LLSDACSE7830:
+	.section	.text$_ZN7CppTestD1Ev,"x"
+	.linkonce discard
+	.section .rdata,"dr"
+LC1:
+	.ascii "Hello world\0"
+	.section	.text$_ZN7CppTest4FunAEv,"x"
+	.linkonce discard
+	.align 2
+	.globl	__ZN7CppTest4FunAEv
+	.def	__ZN7CppTest4FunAEv;	.scl	2;	.type	32;	.endef
+__ZN7CppTest4FunAEv:
+LFB7831:
+	.cfi_startproc
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	subl	$40, %esp
+	movl	%ecx, -12(%ebp)
+	movl	-12(%ebp), %eax
+	movl	(%eax), %eax
+	movl	%eax, (%esp)
+	movl	$__ZSt4cout, %ecx
+	call	__ZNSolsEi
+	subl	$4, %esp
+	movl	$__ZSt4endlIcSt11char_traitsIcEERSt13basic_ostreamIT_T0_ES6_, (%esp)
+	movl	%eax, %ecx
+	call	__ZNSolsEPFRSoS_E
+	subl	$4, %esp
+	movl	$LC1, 4(%esp)
+	movl	$__ZSt4cout, (%esp)
+	call	__ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc
+	movl	$__ZSt4endlIcSt11char_traitsIcEERSt13basic_ostreamIT_T0_ES6_, (%esp)
+	movl	%eax, %ecx
+	call	__ZNSolsEPFRSoS_E
+	subl	$4, %esp
+	nop
+	leave
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+LFE7831:
+	.def	___main;	.scl	2;	.type	32;	.endef
+	.text
+	.globl	_main
+	.def	_main;	.scl	2;	.type	32;	.endef
+_main:
+LFB7833:
+	.cfi_startproc
+	leal	4(%esp), %ecx
+	.cfi_def_cfa 1, 0
+	andl	$-16, %esp
+	pushl	-4(%ecx)
+	pushl	%ebp
+	.cfi_escape 0x10,0x5,0x2,0x75,0
+	movl	%esp, %ebp
+	pushl	%ebx
+	pushl	%ecx
+	.cfi_escape 0xf,0x3,0x75,0x78,0x6
+	.cfi_escape 0x10,0x3,0x2,0x75,0x7c
+	subl	$32, %esp
+	call	___main
+	movl	$4, (%esp)
+	call	__Znwj
+	movl	%eax, %ebx
+	movl	$2, (%esp)
+	movl	%ebx, %ecx
+	call	__ZN7CppTestC1Ei
+	subl	$4, %esp
+	movl	%ebx, -12(%ebp)
+	movl	-12(%ebp), %eax
+	movl	%eax, %ecx
+	call	__ZN7CppTest4FunAEv
+	movl	-12(%ebp), %ebx
+	testl	%ebx, %ebx
+	je	L5
+	movl	%ebx, %ecx
+	call	__ZN7CppTestD1Ev
+	movl	$4, 4(%esp)
+	movl	%ebx, (%esp)
+	call	__ZdlPvj
+L5:
+	movl	$0, %eax
+	leal	-8(%ebp), %esp
+	popl	%ecx
+	.cfi_restore 1
+	.cfi_def_cfa 1, 0
+	popl	%ebx
+	.cfi_restore 3
+	popl	%ebp
+	.cfi_restore 5
+	leal	-4(%ecx), %esp
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+LFE7833:
+	.def	___tcf_0;	.scl	3;	.type	32;	.endef
+___tcf_0:
+LFB8527:
+	.cfi_startproc
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	subl	$8, %esp
+	movl	$__ZStL8__ioinit, %ecx
+	call	__ZNSt8ios_base4InitD1Ev
+	leave
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+LFE8527:
+	.def	__Z41__static_initialization_and_destruction_0ii;	.scl	3;	.type	32;	.endef
+__Z41__static_initialization_and_destruction_0ii:
+LFB8526:
+	.cfi_startproc
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	subl	$24, %esp
+	cmpl	$1, 8(%ebp)
+	jne	L10
+	cmpl	$65535, 12(%ebp)
+	jne	L10
+	movl	$__ZStL8__ioinit, %ecx
+	call	__ZNSt8ios_base4InitC1Ev
+	movl	$___tcf_0, (%esp)
+	call	_atexit
+L10:
+	nop
+	leave
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+LFE8526:
+	.def	__GLOBAL__sub_I_main;	.scl	3;	.type	32;	.endef
+__GLOBAL__sub_I_main:
+LFB8528:
+	.cfi_startproc
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	subl	$24, %esp
+	movl	$65535, 4(%esp)
+	movl	$1, (%esp)
+	call	__Z41__static_initialization_and_destruction_0ii
+	leave
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+LFE8528:
+	.section	.ctors,"w"
+	.align 4
+	.long	__GLOBAL__sub_I_main
+	.ident	"GCC: (MinGW.org GCC Build-2) 9.2.0"
+	.def	__ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc;	.scl	2;	.type	32;	.endef
+	.def	__ZSt4endlIcSt11char_traitsIcEERSt13basic_ostreamIT_T0_ES6_;	.scl	2;	.type	32;	.endef
+	.def	__ZNSolsEPFRSoS_E;	.scl	2;	.type	32;	.endef
+	.def	__ZNSolsEi;	.scl	2;	.type	32;	.endef
+	.def	__Znwj;	.scl	2;	.type	32;	.endef
+	.def	__ZdlPvj;	.scl	2;	.type	32;	.endef
+	.def	__ZNSt8ios_base4InitD1Ev;	.scl	2;	.type	32;	.endef
+	.def	__ZNSt8ios_base4InitC1Ev;	.scl	2;	.type	32;	.endef
+	.def	_atexit;	.scl	2;	.type	32;	.endef
