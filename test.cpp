a2414512ca76ac86a4aa36646d4bda0eba7129db
FcAYH <1473988037@qq.com>
2022-09-06 11:03:19 +0800

    什么时候c++可以用变量声明数组大小了

--- test.cpp
+++ test.cpp
@@ -2,57 +2,17 @@
 
 using namespace std;
 
-class Test
-{
-public:
-    int val;
-    char *str;
-
-public:
-    Test(int x) : val(x)
-    {
-        cout << "ctor" << endl;
-    }
-
-    ~Test()
-    {
-        cout << "val: " << val << " free" << endl;
-    }
-
-    void ctor()
-    {
-        str = (char *)malloc(sizeof(char) * 6);
-        cin >> str;
-    }
-
-    void fun1()
-    {
-        delete this;
-        cout << "hello world" << endl;
-    }
-
-    virtual void fun2()
-    {
-        cout << "virtual fun" << endl;
-    }
-};
-
 void Solve()
 {
-    // Test *p = (Test *)malloc(sizeof(p));
-    // p->ctor();
-    // free(p);
-    // cout << p->str;
-    // Test *p = new Test(2);
-    // delete p;
-    // p->fun1();
-    // delete p;
+    int n;
+    scanf("%d", &n);
 
-    Test *q = nullptr;
-    // cout << q->val << endl;
-    q->fun1();
-    q->fun2();
-    delete q;
+    int a[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", a[i]);
+    }
 }
 
 int main()
